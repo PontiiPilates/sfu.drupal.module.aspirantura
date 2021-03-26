c7c587beea3365c41827a5fc1bfa702e325e15dc
zloileshii <zloileshii@gmail.com>
2021-03-26 09:11:35 +0700

    Правка первого таба третьей страницы для отсутствующих мест набора

--- aspirantura.js
+++
@@ -1,5 +0,0 @@
-$('.tab-btn').bind('click', function(evt) {
-    evt.preventDefault();
-    $(this).addClass('tab-active');
-    // alert('Ok!');
-})
---
+++ navigation.php
@@ -0,0 +1,41 @@
+<div class="pills-box">
+    <a href="#" class="pill">Аспиранты</a>
+    <a href="#" class="pill">Образовательные программы</a>
+    <a href="#" class="pill">Образовательные программы</a>
+    <a href="#" class="pill">Научнеые руководители</a>
+    <a href="#" class="pill">Научнеые руководители</a>
+    <a href="#" class="pill">Аспиранты</a>
+    <a href="#" class="pill">Докторанты</a>
+    <a href="#" class="pill">Физруки</a>
+</div>
+
+<style>
+    .pills-box {
+        display: flex;
+        max-width: 960px;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin: -8px;
+    }
+
+    .pill {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+        height: 3em;
+        width: 200;
+        color: black;
+        transition: 0.25s;
+        border-radius: 30px;
+        border: 1px solid;
+        margin: 8px;
+        text-decoration: none;
+    }
+
+    .pill:hover {
+        border-color: orangered;
+        color: #fff;
+        box-shadow: inset 241px 0 0 0 orangered;
+    }
+</style>
